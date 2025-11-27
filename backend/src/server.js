48af1d23eaf1f93868d7b60e538cc5fc8947ae6a
Arman2337 <23it119@charusat.edu.in>
2025-11-27 20:06:09 +0530

    update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import http from "http";
 import app from "./app.js";
 import { initNotificationSocket } from "./sockets/notifications.js";
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
